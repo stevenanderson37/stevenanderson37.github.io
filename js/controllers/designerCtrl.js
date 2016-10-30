a5c67daf4106b43a550590a8ecb585f7496dff7d
Steven Anderson <stevenanderson@Stevens-MacBook-Pro.local>
2016-10-30 05:33:40 -0600

    sticky titles and descriptions figured out

--- js/controllers/designerCtrl.js
+++ js/controllers/designerCtrl.js
@@ -1,4 +1,20 @@
 angular.module('portfolioApp')
 .controller('designerCtrl', function($scope, $stateParams, portfolioSrv) {
   $scope.stateName = 'Designer Portfolio';
+
+  $("#section1-text").stick_in_parent({
+    // "parent": ".intro-text-box",
+    "bottoming": false
+  });
+
+  $("#section2-text").stick_in_parent({
+    // "parent": ".intro-text-box",
+    "bottoming": false
+  });
+
+  $("#section3-text").stick_in_parent({
+    // "parent": ".intro-text-box",
+    "bottoming": false
+  });
+  
 })
--- js/controllers/developerCtrl.js
+++ js/controllers/developerCtrl.js
@@ -1,26 +1,24 @@
 angular.module('portfolioApp')
 .controller('developerCtrl', function($scope, $stateParams, portfolioSrv) {
 
-  // $('#section1-title').fixTo('#section1');
-  // $('#section2-title').fixTo('#section2');
-  // $('#section3-title').fixTo('#section3');
+  // $(".sticky-position-box").stick_in_parent({
+  //   "bottoming": false
+  // });
 
-  // var titleOffset = $("#section1").offset().top;
-  //
-  // $("#section1").wrap('<div class="title-placeholder"></div>');
-  // $(".title-placeholder").height($("#section1-title").outerHeight());
-  //
-  // $(window).scroll(function() {
-  //   var scrollPos = $(window).scrollTop();
-  //
-  //   if (scrollPos >= titleOffset && scrollPos <= removeOffset) {
-  //     $("#section1").addClass("fixed");
-  //   } else {
-  //     $("#section1").removeClass("fixed");
-  //   }
-  // })
+  $("#section1-text").stick_in_parent({
+    // "parent": ".intro-text-box",
+    "bottoming": false
+  });
 
+  $("#section2-text").stick_in_parent({
+    // "parent": ".intro-text-box",
+    "bottoming": false
+  });
 
+  $("#section3-text").stick_in_parent({
+    // "parent": ".intro-text-box",
+    "bottoming": false
+  });
 
 
 });
