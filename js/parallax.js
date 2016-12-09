25c6eb7e698ed8a4cf80020cd13667b80a1b7566
Steven Anderson <happygoatdesigns@gmail.com>
2016-12-09 14:41:17 -0700

    jquery parallax removed from header when viewed on a screen as small as a tablet or smaller

--- js/parallax.js
+++ js/parallax.js
@@ -1,82 +1,86 @@
 $(window).scroll(function() {
 
-  // find the scroll and use this variable to move elements
+  if ($(window).width() >= 1026) {
 
-  var winScroll = $(this).scrollTop();
-  // console.log(winScroll);
+    // find the scroll and use this variable to move elements
 
-  // left side moves out of view on the x-axis on scroll
+    var winScroll = $(this).scrollTop();
+    // console.log(winScroll);
 
-  $('.home-earbuds').css({
-    'transform': 'rotate(.625turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    // left side moves out of view on the x-axis on scroll
 
-  $('.home-earbuds-shadow').css({
-    'transform': 'rotate(.625turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    $('.home-earbuds').css({
+      'transform': 'rotate(.625turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  $('.home-bamboo').css({
-    'transform': 'rotate(.625turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    $('.home-earbuds-shadow').css({
+      'transform': 'rotate(.625turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  $('.home-bamboo-shadow').css({
-    'transform': 'rotate(.625turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    $('.home-bamboo').css({
+      'transform': 'rotate(.625turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  // right side moves out of view on the x-axis on scroll
+    $('.home-bamboo-shadow').css({
+      'transform': 'rotate(.625turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  $('.home-ballpoint').css({
-    'transform': 'rotate(.91turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    // right side moves out of view on the x-axis on scroll
 
-  $('.home-ballpoint-shadow').css({
-    'transform': 'rotate(.91turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    $('.home-ballpoint').css({
+      'transform': 'rotate(.91turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  $('.home-felt-pen').css({
-    'transform': 'rotate(.655turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    $('.home-ballpoint-shadow').css({
+      'transform': 'rotate(.91turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  $('.home-felt-pen-shadow').css({
-    'transform': 'rotate(.655turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    $('.home-felt-pen').css({
+      'transform': 'rotate(.655turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  $('.home-sharpie').css({
-    'transform': 'rotate(.9turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    $('.home-felt-pen-shadow').css({
+      'transform': 'rotate(.655turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  $('.home-sharpie-shadow').css({
-    'transform': 'rotate(.9turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
-  });
+    $('.home-sharpie').css({
+      'transform': 'rotate(.9turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  $('.home-mouse').css({
-    'transform': 'rotate(.9turn) translate('+ winScroll /7 +'%, '+ winScroll /10 +'%)'
-  });
+    $('.home-sharpie-shadow').css({
+      'transform': 'rotate(.9turn) translate('+ winScroll /13 +'%, -'+ winScroll /10 +'%)'
+    });
 
-  $('.home-mouse-shadow').css({
-    'transform': 'rotate(.9turn) translate('+ winScroll /7 +'%, '+ winScroll /10 +'%)'
-  });
+    $('.home-mouse').css({
+      'transform': 'rotate(.9turn) translate('+ winScroll /7 +'%, '+ winScroll /10 +'%)'
+    });
 
-  // center moves down on the y-axis on scroll
+    $('.home-mouse-shadow').css({
+      'transform': 'rotate(.9turn) translate('+ winScroll /7 +'%, '+ winScroll /10 +'%)'
+    });
 
-  $('.logo').css({
-    'transform': 'translate(0px, '+ winScroll /20 +'%)'
-  });
+    // center moves down on the y-axis on scroll
 
-  $('.home-keyboard').css({
-    'transform': 'translate(0px, '+ winScroll /5 +'%)'
-  });
+    $('.logo').css({
+      'transform': 'translate(0px, '+ winScroll /20 +'%)'
+    });
 
-  $('.home-keyboard-shadow').css({
-    'transform': 'translate(0px, '+ winScroll /5 +'%)'
-  });
+    $('.home-keyboard').css({
+      'transform': 'translate(0px, '+ winScroll /5 +'%)'
+    });
 
-  $('.home-iMac-above').css({
-    'transform': 'translate(0px, -'+ winScroll /10 +'%)'
-  });
+    $('.home-keyboard-shadow').css({
+      'transform': 'translate(0px, '+ winScroll /5 +'%)'
+    });
 
-  $('.home-iMac-above-shadow').css({
-    'transform': 'translate(0px, -'+ winScroll /10 +'%)'
-  });
+    $('.home-iMac-above').css({
+      'transform': 'translate(0px, -'+ winScroll /10 +'%)'
+    });
+
+    $('.home-iMac-above-shadow').css({
+      'transform': 'translate(0px, -'+ winScroll /10 +'%)'
+    });
+
+  }
 
 });
