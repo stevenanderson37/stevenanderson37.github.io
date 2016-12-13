0d1e02d4b2f902437002118a392e8ed06b1a6266
Steven Anderson <happygoatdesigns@gmail.com>
2016-12-13 10:03:50 -0700

    made media query to allow content to look better on vertical smart phones. Still needs more work though

--- js/controllers/designerCtrl.js
+++ js/controllers/designerCtrl.js
@@ -2,19 +2,21 @@ angular.module('portfolioApp')
 .controller('designerCtrl', function($scope, $stateParams, portfolioSrv) {
   $scope.stateName = 'Designer Portfolio';
 
-  $("#section1-text").stick_in_parent({
-    // "parent": ".intro-text-box",
-    "bottoming": false
-  });
+  if ($(window).width() >= 850) {
+    $("#section1-text").stick_in_parent({
+      // "parent": ".intro-text-box",
+      "bottoming": false
+    });
 
-  $("#section2-text").stick_in_parent({
-    // "parent": ".intro-text-box",
-    "bottoming": false
-  });
+    $("#section2-text").stick_in_parent({
+      // "parent": ".intro-text-box",
+      "bottoming": false
+    });
 
-  $("#section3-text").stick_in_parent({
-    // "parent": ".intro-text-box",
-    "bottoming": false
-  });
+    $("#section3-text").stick_in_parent({
+      // "parent": ".intro-text-box",
+      "bottoming": false
+    });
+  }
 
 })
--- js/controllers/developerCtrl.js
+++ js/controllers/developerCtrl.js
@@ -2,10 +2,12 @@ angular.module('portfolioApp')
 .controller('developerCtrl', function($scope, $stateParams, portfolioSrv) {
   $scope.stateName = 'Developer Portfolio';
 
-  $(".sticky-position-box").stick_in_parent({
-    // "parent": ".intro-text-box",
-    "bottoming": false
-  });
+  if ($(window).width() >= 850) {
+    $(".sticky-position-box").stick_in_parent({
+      "parent": ".intro-text-box",
+      "bottoming": false
+    });
+  }
 
   // $("#section1-text").stick_in_parent({
   //   // "parent": ".intro-text-box",
