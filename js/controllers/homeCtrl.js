361f7ed3d6c11819fe303c837ae85d4c855a043f
Steven Anderson <happygoatdesigns@gmail.com>
2017-09-03 23:46:08 -0600

    adds some jquery to add a box-shadow and margin to gifs to better match the png files. still needs a little work, but looks better.

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -192,19 +192,88 @@ angular.module('portfolioApp')
       });
 
       // vv JQUERY MAC CLONE IMAGE SLIDER vv //
+      $(function() {
+        var currentImg = $('.active-mac');
+        var nextImg = currentImg.next();
+        var imgSrc = document.getElementById('current-mac').src;
+
+        if (currentImg.length && /gif/.test(imgSrc)) {
+          currentImg.addClass('box-shadow-gif').css('box-shadow', '0px 0px 10px rgba(0,0,0,.3)');
+          currentImg.addClass('box-shadow-gif').css('margin', 15+'px');
+          currentImg.removeAttr('id');
+          nextImg.attr('id', 'current-mac');
+          imgSrc = document.getElementById('current-mac').src;
+        } else {
+          currentImg.removeClass('box-shadow-gif');
+          currentImg.removeAttr('id');
+          nextImg.attr('id', 'current-mac');
+          imgSrc = "";
+        }
+      });
+
+      $(function() {
+        var currentImg = $('.active-paul');
+        var nextImg = currentImg.next();
+        var imgSrc = document.getElementById('current-paul').src;
+
+        if (currentImg.length && /gif/.test(imgSrc)) {
+          currentImg.addClass('box-shadow-gif').css('box-shadow', '0px 0px 10px rgba(0,0,0,.3)');
+          currentImg.addClass('box-shadow-gif').css('margin', 15+'px');
+          currentImg.removeAttr('id');
+          nextImg.attr('id', 'current-paul');
+          imgSrc = document.getElementById('current-paul').src;
+        } else {
+          currentImg.removeClass('box-shadow-gif');
+          currentImg.removeAttr('id');
+          nextImg.attr('id', 'current-paul');
+          imgSrc = "";
+        }
+      });
+
+      $(function() {
+        var currentImg = $('.active-design-port');
+        var nextImg = currentImg.next();
+        var imgSrc = document.getElementById('current-design-port').src;
+
+        if (currentImg.length && /gif|png|jpg|jpeg/.test(imgSrc)) {
+          currentImg.addClass('box-shadow-gif').css('box-shadow', '0px 0px 10px rgba(0,0,0,.3)');
+          currentImg.addClass('box-shadow-gif').css('margin', 15+'px');
+          currentImg.removeAttr('id');
+          nextImg.attr('id', 'current-design-port');
+          imgSrc = document.getElementById('current-design-port').src;
+        } else {
+          currentImg.removeClass('box-shadow-gif');
+          currentImg.removeAttr('id');
+          nextImg.attr('id', 'current-design-port');
+          imgSrc = "";
+        }
+      });
 
       $(function() {
         $('.next-mac').on('click', function(){
           var currentImg = $('.active-mac');
           var nextImg = currentImg.next();
+          var imgSrc = document.getElementById('current-mac').src;
 
-          if(nextImg.length){
+          if(nextImg.length) {
             currentImg.removeClass('active-mac').css('z-index', -10);
             nextImg.addClass('active-mac').css('z-index', 10);
           }
+
+          if (nextImg.length && /gif/.test(imgSrc)) {
+            nextImg.addClass('box-shadow-gif').css('box-shadow', '0px 0px 10px rgba(0,0,0,.3)');
+            nextImg.addClass('box-shadow-gif').css('margin', 15+'px');
+            currentImg.removeAttr('id');
+            nextImg.attr('id', 'current-mac');
+          } else if (nextImg.length && /png|jpg|jpeg/.test(imgSrc)) {
+            currentImg.addClass('box-shadow-gif').css('margin', 0+'px');
+            currentImg.addClass('box-shadow-gif').css('box-shadow', 'none');
+            currentImg.removeAttr('id');
+            nextImg.attr('id', 'current-mac');
+          }
         });
 
-        $('.prev-mac').on('click', function(){
+        $('.prev-mac').on('click', function() {
           var currentImg = $('.active-mac');
           var prevImg = currentImg.prev();
 
@@ -218,21 +287,34 @@ angular.module('portfolioApp')
       // vv JQUERY PAUL VALENTINE IMAGE SLIDER vv //
 
       $(function() {
-        $('.next-paul').on('click', function(){
+        $('.next-paul').on('click', function() {
           var currentImg = $('.active-paul');
           var nextImg = currentImg.next();
+          var imgSrc = document.getElementById('current-mac').src;
 
-          if(nextImg.length){
+          if(nextImg.length) {
             currentImg.removeClass('active-paul').css('z-index', -10);
             nextImg.addClass('active-paul').css('z-index', 10);
           }
+
+          if (nextImg.length && /gif/.test(imgSrc)) {
+            nextImg.addClass('box-shadow-gif').css('box-shadow', '0px 0px 10px rgba(0,0,0,.3)');
+            nextImg.addClass('box-shadow-gif').css('margin', 15+'px');
+            currentImg.removeAttr('id');
+            nextImg.attr('id', 'current-mac');
+          } else if (nextImg.length && /png|jpg|jpeg/.test(imgSrc)) {
+            currentImg.addClass('box-shadow-gif').css('margin', 0+'px');
+            currentImg.addClass('box-shadow-gif').css('box-shadow', 'none');
+            currentImg.removeAttr('id');
+            nextImg.attr('id', 'current-mac');
+          }
         });
 
-        $('.prev-paul').on('click', function(){
+        $('.prev-paul').on('click', function() {
           var currentImg = $('.active-paul');
           var prevImg = currentImg.prev();
 
-          if(prevImg.length){
+          if(prevImg.length) {
             currentImg.removeClass('active-paul').css('z-index', -10);
             prevImg.addClass('active-paul').css('z-index', 10);
           }
@@ -242,21 +324,34 @@ angular.module('portfolioApp')
       // vv JQUERY DESIGN PORTFOLIO IMAGE SLIDER vv //
 
       $(function() {
-        $('.next-design-port').on('click', function(){
+        $('.next-design-port').on('click', function() {
           var currentImg = $('.active-design-port');
           var nextImg = currentImg.next();
+          var imgSrc = document.getElementById('current-mac').src;
 
-          if(nextImg.length){
+          if(nextImg.length) {
             currentImg.removeClass('active-design-port').css('z-index', -10);
             nextImg.addClass('active-design-port').css('z-index', 10);
           }
+
+          if (nextImg.length && /gif/.test(imgSrc)) {
+            nextImg.addClass('box-shadow-gif').css('box-shadow', '0px 0px 10px rgba(0,0,0,.3)');
+            nextImg.addClass('box-shadow-gif').css('margin', 15+'px');
+            currentImg.removeAttr('id');
+            nextImg.attr('id', 'current-mac');
+          } else if (nextImg.length && /png|jpg|jpeg/.test(imgSrc)) {
+            currentImg.addClass('box-shadow-gif').css('margin', 0+'px');
+            currentImg.addClass('box-shadow-gif').css('box-shadow', 'none');
+            currentImg.removeAttr('id');
+            nextImg.attr('id', 'current-mac');
+          }
         });
 
-        $('.prev-design-port').on('click', function(){
+        $('.prev-design-port').on('click', function() {
           var currentImg = $('.active-design-port');
           var prevImg = currentImg.prev();
 
-          if(prevImg.length){
+          if(prevImg.length) {
             currentImg.removeClass('active-design-port').css('z-index', -10);
             prevImg.addClass('active-design-port').css('z-index', 10);
           }
